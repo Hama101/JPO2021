fca509957c0821579df819057132153ff3c1d969
hama <HamdiAAA>
2021-11-02 20:33:13 +0100

    test

--- Qrs/settings.py
+++ Qrs/settings.py
@@ -121,11 +121,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-DISABLE_COLLECTSTATIC=1
+DISABLE_COLLECTSTATIC=True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
