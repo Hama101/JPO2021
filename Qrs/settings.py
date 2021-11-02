e5f0b1843bd654fc0a72af22c119580e68bc5a97
hama <HamdiAAA>
2021-11-02 20:25:19 +0100

    staticfiles

--- Qrs/settings.py
+++ Qrs/settings.py
@@ -121,6 +121,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+DISABLE_COLLECTSTATIC=1
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
